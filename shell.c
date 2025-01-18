e6e286f96b63e28db44b2a4337aa1808059509f6
Rishabh <rishabhprsd@proton.me>
2025-01-18 16:46:43 +0530

    Add ls

--- shell.c
+++ shell.c
@@ -1,16 +1,50 @@
-#include <stdio.h>
-#include <string.h>
+#include <dirent.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+
 
 #define MAX_SIZE 100
+
+int built_ins(char *command) {
+	if(command==NULL) 
+		return -1;
+
+	if (strcmp(command,"ls")==0) {
+		DIR *dir;
+		struct dirent *entry;
+		dir = opendir(".");
+		if(dir==NULL) {
+			perror("Unable to open directory");
+			return 1;
+		}
+		
+		while((entry= readdir(dir)) != NULL) {
+			printf("%s\n", entry->d_name);
+		}
+	}
+
+
+	return 0;
+}
+
+
 
 void repl() {
 	while (true) {
 		printf("$ ");
 		char buffer[MAX_SIZE];
 		if(fgets(buffer, MAX_SIZE, stdin)!=NULL){
-			printf("%s", buffer);
+        		size_t len = strlen(buffer);
+        		if (len > 0 && buffer[len - 1] == '\n') {
+            			buffer[len - 1] = '\0';
+        		}
+
+			int val= built_ins(buffer);
+			if (val==2)
+				break;
 		}
 		else {
 			if(feof(stdin))
