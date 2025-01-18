475af1da97c19af3f4bd8928dedf45f919d01530
Rishabh <rishabhprsd@proton.me>
2025-01-18 20:01:48 +0530

    add cd exit help and parser

--- shell.c
+++ shell.c
@@ -2,13 +2,15 @@
 #include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
 
 
 #define MAX_SIZE 100
+#define TOKEN_COUNT 10
 
-int built_ins(char *command) {
+int built_ins(char *command, const char *arg) {
 	if(command==NULL) 
 		return -1;
 
@@ -25,9 +27,47 @@ int built_ins(char *command) {
 			printf("%s\n", entry->d_name);
 		}
 	}
+	else if (strcmp(command,"cd")==0) {
+		char new_dir[40];
+		if(arg==NULL)
+			strncpy(new_dir, "home/rishabh", 40);
+		else {
+			strncpy(new_dir, arg, 40);
+		}
+
+		int status= chdir(new_dir);
+		if(status!=0){
+			perror("changing directory failed");
+			return 1;
+		}
+		
+	}
+	else if (strcmp(command,"exit")==0) {
+		return 2;
+	}
+	else if(strcmp(command,"help")==0) {
+		printf("Steel Shell, version 0.1\n");
+		printf("Following are the inbuilt commands of steelshell:\n");
+		printf("1. ls: Lists files and directories in the current directory.\n");
+		printf("2. cd arg: Switches current directory to arg\n");
+		printf("3. exit: Exits the shell\n");
+		printf("4. help: Prints out this message\n");
+	}
 
 
 	return 0;
+}
+
+char** parse(char *str) {
+	char **tokens = (char **)malloc(TOKEN_COUNT*sizeof(char*));
+	tokens[0]= strtok(str, " ");
+	for (int i=1;; i++){
+		if(tokens[i-1]==NULL)
+			break;
+		tokens[i] = strtok(NULL, " ");
+	}
+
+	return tokens;
 }
 
 
@@ -42,9 +82,19 @@ void repl() {
             			buffer[len - 1] = '\0';
         		}
 
-			int val= built_ins(buffer);
-			if (val==2)
-				break;
+			char **tokens;
+			tokens = parse(buffer);
+			if (tokens[0]==NULL)
+				continue;
+
+			if(strcmp(tokens[0],"ls")==0 ||
+			   strcmp(tokens[0],"cd")==0 ||
+			   strcmp(tokens[0],"exit")==0 ||
+			   strcmp(tokens[0],"help")==0) {
+				int val= built_ins(tokens[0], tokens[1]);
+				if (val==2)
+					break;
+			}
 		}
 		else {
 			if(feof(stdin))
