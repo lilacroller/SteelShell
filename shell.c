970423b0e546728f7d856010471c054d7e483918
Rishabh <rishabhprsd@proton.me>
2025-01-21 23:00:49 +0530

    add redirection support

--- shell.c
+++ shell.c
@@ -1,5 +1,7 @@
+#include <assert.h>
 #include <dirent.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,10 +12,32 @@
 
 #define MAX_SIZE 100
 #define TOKEN_COUNT 10
-
-int built_ins(char *command, const char *arg) {
+#define INPUT_FD 0
+#define OUTPUT_FD 1
+int built_ins(char *command, const char *arg, char *infile, char *outfile) {
 	if(command==NULL) 
 		return -1;
+
+	int saved_infd = dup(0);
+	int saved_outfd = dup(1);
+	int infileid;
+	int outfileid;
+	if(infile!=NULL || outfile!=NULL) {
+		if(outfile != NULL) {
+			if((outfileid= open(outfile, O_CREAT | O_RDWR, S_IRWXU))==-1) {
+				perror("unable to open file");
+				return -1;
+			}
+			dup2(outfileid, OUTPUT_FD);
+		}
+		if (infile != NULL) {
+			if((infileid= open(infile, O_RDONLY | O_RDWR, S_IRWXU))==-1) {
+				perror("unable to open file");
+				return -1;
+			}
+			dup2(infileid, INPUT_FD);
+		}
+	}
 
 	if (strcmp(command,"ls")==0) {
 		DIR *dir;
@@ -25,8 +49,11 @@ int built_ins(char *command, const char *arg) {
 		}
 		
 		while((entry= readdir(dir)) != NULL) {
-			printf("%s\n", entry->d_name);
+			printf("%s ", entry->d_name);
 		}
+
+		closedir(dir);
+		printf("\n");
 	}
 	else if (strcmp(command,"cd")==0) {
 		char new_dir[40];
@@ -55,23 +82,30 @@ int built_ins(char *command, const char *arg) {
 		printf("4. help: Prints out this message\n");
 	}
 
+	if(infile!=NULL || outfile!=NULL){
+		if(outfile!=NULL){
+			dup2(saved_outfd, 1);
+			close(outfileid);
+		} 
+		else if(infile!=NULL)  {
+			dup2(saved_infd, 0);
+			close(infileid);
+		}
+	}
+	
 
 	return 0;
 }
 
 
-typedef struct{
-	char **tokens;
-	int size;
-} token_pack;
 
-int exec_user_program(token_pack arg_pack) {
-	char **args= arg_pack.tokens;
-	int n= arg_pack.size;
-
-	if(n>=TOKEN_COUNT)
+int exec_user_program(char **args, size_t n, char *infile, char *outfile) {
+	if(n>=TOKEN_COUNT){
 		printf("no. of args exceeded\n");
-	
+		return -1;
+	}
+
+//	printf("n: %zu\n", n);
 	args[n]= NULL;
 	pid_t pid;
 	if ((pid= vfork())<0){
@@ -79,6 +113,33 @@ int exec_user_program(token_pack arg_pack) {
 		return -1;
 	}
 	else if (pid==0) {
+		if (infile!=NULL || outfile!=NULL){
+			int infileid, outfileid;
+			if(outfile!= NULL){
+				if((outfileid = open(outfile, O_CREAT | O_RDWR, S_IRWXU))==-1){
+					perror("failed to open file");
+				}
+
+				if(dup2(outfileid, 1)==-1){
+					perror("Failed to duplicate the fd");
+				}
+				close(outfileid);
+			}
+			if(infile!=NULL){
+				if((infileid = open(infile, O_CREAT | O_RDWR, S_IRWXU))==-1){
+					perror("failed to open file");
+				}
+				
+				if(dup2(infileid, 0)==-1){
+					perror("Failed to duplicate the fd");
+				}
+				close(infileid);
+			}
+		}
+		
+//		for (int i =0; i<n; i++)
+//			printf("%s ", args[i]);
+
 		if(execv(args[0], args)==-1)
 			perror("execv failed");
 	}
@@ -86,6 +147,11 @@ int exec_user_program(token_pack arg_pack) {
 	wait(NULL);
 	return 0;
 }
+
+typedef struct{
+	char **tokens;
+	int size;
+} token_pack;
 
 token_pack parse(char *str) {
 	char **tokens = (char **)malloc(TOKEN_COUNT*sizeof(char*));
@@ -100,8 +166,8 @@ token_pack parse(char *str) {
 	}
 	token_pack pack={tokens,n};
 	return pack;
-}
 
+}
 
 
 void repl() {
@@ -118,6 +184,30 @@ void repl() {
 			token_pack unpack;
 			unpack = parse(buffer);
 			args= unpack.tokens;
+			int n = unpack.size;
+
+
+			char *infile= NULL;
+			char *outfile= NULL;
+			size_t newn= 1;
+			int flag=0;	
+
+			assert(n<TOKEN_COUNT);
+
+			for (int i =0; i<n; i++){
+				if(strcmp(args[i],">")==0){
+					outfile= args[i+1];
+					flag = 1;
+				}
+				else if(strcmp(args[i],"<")==0){
+					infile = args[i+1];
+					flag = 1;
+				}
+
+				if(flag==0)
+					newn= i+1;
+			}
+
 
 			if (args[0]==NULL)
 				continue;
@@ -126,21 +216,26 @@ void repl() {
 			   strcmp(args[0],"cd")==0 ||
 			   strcmp(args[0],"exit")==0 ||
 			   strcmp(args[0],"help")==0) {
-				int val= built_ins(args[0], args[1]);
-				if (val==2)
+				int val= built_ins(args[0], args[1], infile, outfile);
+				free(unpack.tokens);	
+				if (val==2){
 					break;
+				}
 			}
 			else {
-				int val= exec_user_program(unpack);
+				int val= exec_user_program(args, newn, infile, outfile);
+				free(unpack.tokens);	
 				if(val==-1)
 					printf("failed to execute the program\n");
 			}
 		}
 		else {
-			if(feof(stdin))
+			if(feof(stdin)){
 				break;
+			}
 			printf("command not found\n");
 		}
+
 	}
 }
 
